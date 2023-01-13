9de4ed8732912074d2bbf9d91fdc7466370e79b6
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-13 09:26:23 +0000

    commit tree 2f5fc5895e0eb0e883b5c2b9b0431037d3187f0e

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,26 @@
 function fibonacci(num) {
-  // type your code here
+
+
+  // First two numbers are unique
+  let fibArr = [0, 1]
+
+  if (num <= 1){
+    return fibArr[num]
+  }
+
+  // subsequent numbers are predictable
+
+  for (let i = 2; i <= num; i ++){
+
+    fibArr.push(fibArr[i-1] + fibArr[i-2])
+    
+  }
+
+  return fibArr[num]
+
 }
+
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -22,3 +42,4 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
